f6506229d71a1742232ebdfc942162e79f4edfe0
kido1611 <aci@hotmail.co.id>
2017-01-02 07:56:07 +0700

    Add Tabs

--- src/Base.js
+++ src/Base.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {indigo500, fullWhite} from 'material-ui/styles/colors';
+import {indigo500, pink500, fullWhite} from 'material-ui/styles/colors';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
 
 class Base extends React.Component{
-	constructor(props){
-		super(props);
+	// constructor(props){
+	// 	super(props);
 
-	}
+	// }
 
 	render(){
 		const styles = {
@@ -14,12 +17,12 @@ class Base extends React.Component{
 				padding: '0px',
 				margin: '0px',
 				position: 'relative',
-				minWidth: '320px',
+				minWidth: '300px',
 			},
 			header:{
 				background: indigo500, 
 				color: fullWhite,
-				height: '140px',
+				height: '160px',
 			},
 			headerContainer: {
 				padding: '16px',
@@ -27,37 +30,66 @@ class Base extends React.Component{
 				margin: 'auto',
 				maxWidth: '900px',
 			},
-			section: {
-				padding: '16px',
+			container: {
 				background: fullWhite, 
 				width: '90%',
 				maxWidth: '900px',
 				margin: 'auto',
 				marginTop: '-30px',
-				boxShadow: '0 1px 6px rgba(0,0,0,0.12), 0 1px 4px rgba(0,0,0,0.12)',
+			},
+			section: {
+				padding: '16px',
 			},
 			title: {
-				fontSize: '42px',
+				fontSize: '56px',
 				fontWeight: 'bold',
 			},
 			subtitle: {
-				fontSize: '18px',
+				fontSize: '24px',
+			},
+			tab: {
+				background: pink500,
+			},
+			inkBar: {
+				backgroundColor: indigo500, 
 			}
 		};
 		return (
-			<div style={styles.body}>
-				<header style={styles.header}>
-					<div style={styles.headerContainer}>
-						<span style={styles.title}>CCC</span>
-						<br/>
-						<span style={styles.subtitle}>C Computer Club</span>
-					</div>
-				</header>
-				<section style={styles.section}>
-					Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia<br/>
-					asdasd
-				</section>
-			</div>
+			<MuiThemeProvider>
+				<div style={styles.body}>
+					<header style={styles.header}>
+						<div style={styles.headerContainer}>
+							<span style={styles.title}>CCC</span>
+							<br/>
+							<span style={styles.subtitle}>C Computer Club</span>
+						</div>
+					</header>
+					<Paper style={styles.container} zDepth={2}>
+						<Tabs inkBarStyle={styles.inkBar}>
+							<Tab label="Home" style={styles.tab}>
+								<div style={styles.section}>
+								<h2 >Tab One</h2>
+								<p>
+								Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia Halo Dunia<br/>
+						asdasd
+								</p>
+								<p>
+								You can put any sort of HTML or react component in here. It even keeps the component state!
+								</p>
+								</div>
+							</Tab>
+							<Tab label="Struktur Organisasi" style={styles.tab}>
+								<div style={styles.section}>
+								<h2>Tab Two</h2>
+								<p>
+								This is another example tab.
+								</p>
+								</div>
+							</Tab>
+						</Tabs>
+					</Paper>
+				</div>
+			</MuiThemeProvider>
 		);
 	}
 }

5719bf2f7b18ad04535d08a43b1073eec5667a02
kido1611 <aci@hotmail.co.id>
2017-01-02 08:24:52 +0700

    Add Select Field

--- src/components/Struktur.js
+++ src/components/Struktur.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+
 class Struktur extends React.Component{
 	
+	constructor(){
+		super();
+		this.state = {
+			value: 2016,
+		}
+	}
+
+	handleChange = (event, index, value) => {
+		this.setState({value});
+	}
+
 	render(){
 		return (
-			<h1>Struktur</h1>
+			<div>
+				<SelectField
+					floatingLabelText="Struktur Pengurus"
+					value={this.state.value}
+					fullWidth={true}
+					onChange={this.handleChange} >
+					<MenuItem value={2015} primaryText="2015-2016" />
+					<MenuItem value={2016} primaryText="2016-2017" />
+					<MenuItem value={2017} primaryText="2017-2018" />
+				</SelectField>
+			</div>
 		);
 	}
 }
